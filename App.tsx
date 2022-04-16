4ed02620fc51188dca5a858adced9ad5fe331276
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-04-16 11:20:21 -0300

    Template

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Movies</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
